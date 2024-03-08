6cf1a6be774c04b3c5a7dec04d04eab9aeffd2c0
donia789 <154069208+donia789@users.noreply.github.com>
2024-03-08 14:26:19 +0100

    adding files

---
+++ main.js
@@ -0,0 +1,66 @@
+// select elements
+var plusBtns = Array.from(document.querySelectorAll('.fa-circle-plus'));
+var minusBtns = Array.from(document.querySelectorAll('.fa-circle-minus'));
+var favs = Array.from(document.querySelectorAll('.fa-heart'))
+var deleteBtns = Array.from(document.querySelectorAll('.fa-trash-can'))
+var cards = Array.from(document.querySelectorAll('.card'))
+
+console.log(cards)
+
+
+// Favorites // Like Buttons
+for ( let fav of favs) (
+    fav.addEventListener("click",function() {
+        if (fav.style.color != "red") {
+            ( fav.style.color = "red")
+        } else {
+            fav.style.color = "rgb(33, 37, 41)"
+        }
+    })
+)
+
+
+// remove btn
+for ( let i in deleteBtns) {
+    deleteBtns[i].addEventListener("click", function(){
+        cards[i].remove()
+        total()
+
+    })
+}
+
+
+// plus btn
+for (let plusBtn of plusBtns){
+    plusBtn.addEventListener("click", function() {
+        plusBtn.nextElementSibling.innerHTML++
+        total()
+    })
+}
+
+
+
+// minus btn
+for(let minusBtn of minusBtns){
+    minusBtn.addEventListener("click",function(){
+        minusBtn.previousElementSibling.innerHTML > 0 ?  minusBtn.previousElementSibling.innerHTML-- : null
+        total()
+    })
+}
+
+
+
+// function total price
+function total() {
+    let qte = Array.from(document.querySelectorAll('.counter'))
+    let price = Array.from(document.querySelectorAll('.unit-price'))
+
+    let s = 0
+    
+    for (let i = 0; i< price.length; i++) {
+        s = s + price[i].innerHTML * qte[i].innerHTML
+    }
+    document.getElementById('total-price').innerHTML = s
+
+
+}
